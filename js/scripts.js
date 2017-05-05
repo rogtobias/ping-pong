540d0d357a55c3106dea30cebad5a5b4572348f2
Roger Tobias <celticspyder14@gmail.com>
2017-05-05 09:37:08 -0700

    adds the function to the backend logic with notes on what needs to happen. Started the logic for remainder of 3 and ping is being outputted correctly.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 $(document).ready(function() {
   $("#pingPong").submit(function(event) {
     event.preventDefault();
-    debugger;
+//get input from user and convert it from a string to a number
     var userInput = parseInt($("input#userInput").val());
-
+//check to make sure it is a number and alert user if it isn't
     if (isNaN(userInput)) {
       alert("Please only enter numbers.");
     } else {
@@ -14,3 +14,21 @@ $(document).ready(function() {
 });
 
 //--------------------------BACKEND LOGIC
+//create function for game start
+playPingPong = function(userInput) {
+  debugger;
+//create empty array for list of numbers and words
+  var output = [];
+//loop through to the number that the user inputted
+  for (var i = 0; i <= userInput; i++) {
+//if number is divisible by 3 no remainder
+    if (userInput % 3 === 0) {
+      output.push("ping");
+    }
+//if number is divisible by 5 no remainder
+    else if (true) {
+
+    }
+//if number is divisible by both 3 & 5 no remainder
+  };
+};
