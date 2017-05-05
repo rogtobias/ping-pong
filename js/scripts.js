1cf3e715e1c2d639386bc24fcb4c68706dd9b443
Roger Tobias <celticspyder14@gmail.com>
2017-05-05 11:04:26 -0700

    adds the logic to the backend for remainder of 5 and 15. Unfortunately there is a problem with 15 not outputting correctly as ping pong. It's only getting ping from the 3 logic. Working on it.

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,16 @@
 $(document).ready(function() {
   $("#pingPong").submit(function(event) {
     event.preventDefault();
-//get input from user and convert it from a string to a number
+      //get input from user and convert it from a string to a number
     var userInput = parseInt($("input#userInput").val());
-//check to make sure it is a number and alert user if it isn't
+      //check to make sure it is a number and alert user if it isn't
     if (isNaN(userInput)) {
       alert("Please only enter numbers.");
     } else {
-      var result = playPingPong(userInput);
+      var userOutput = playPingPong(userInput);
     }
+      //send the output back to the html & user
+    $("#userOutput").text(userOutput);
   });
 });
 
@@ -17,18 +19,28 @@ $(document).ready(function() {
 //create function for game start
 playPingPong = function(userInput) {
   debugger;
-//create empty array for list of numbers and words
+    //create empty array for list of numbers and words
   var output = [];
-//loop through to the number that the user inputted
-  for (var i = 0; i <= userInput; i++) {
-//if number is divisible by 3 no remainder
-    if (userInput % 3 === 0) {
+    //loop through to the number that the user inputted
+  for (var i = 1; i <= userInput; i++) {
+    //if number is divisible by 3 no remainder
+    if (i % 3 === 0) {
       output.push("ping");
     }
-//if number is divisible by 5 no remainder
-    else if (true) {
-
+      //if number is divisible by 5 no remainder
+    else if (i % 5 === 0) {
+      output.push("pong");
     }
-//if number is divisible by both 3 & 5 no remainder
+      //if number is divisible by both 3 & 5 no remainder
+    else if (i % 15 === 0) {
+      output.push("ping pong");
+    }
+      //else push the number to output
+    else {
+      output.push(i);
+    }
+
   };
+  //return the result back to the frontend
+  return output.join(" ");
 };
